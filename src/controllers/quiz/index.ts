c5aa82dde3ad2b9c7dff2d87109576ff2b8cdab6
jainl28patel <jainilpatel2003@gmail.com>
2023-02-13 17:45:54 +0530

    implement update quiz

--- src/controllers/quiz/index.ts
+++ src/controllers/quiz/index.ts
@@ -1,1 +1,2 @@
 export { default as createQuiz } from './createQuiz'
+export { default as updateQuiz } from './updateQuiz'
--- src/controllers/quiz/updateQuiz.ts
+++ src/controllers/quiz/updateQuiz.ts
@@ -1,7 +1,9 @@
 import sendInvalidInputResponse from '@utils/invalidInputResponse'
 import { Request, Response } from 'express'
+import QuizModel from '@models/quiz/quizModel'
 import { Types } from 'mongoose'
 import { IQuiz } from 'types'
+import sendFailureResponse from '@utils/failureResponse'
 
 interface updateQuizRequest extends Request {
   body: {
@@ -21,8 +23,48 @@ const updateQuiz = async (req: updateQuizRequest, res: Response) => {
     return sendInvalidInputResponse(res)
   }
 
-  // TODO: Implement update quiz
-  res.send('Update quiz')
+  // get the data from the request body
+  const { managers, isAcceptingAnswers, quizMetadata, registrationMetadata, userId } = req.body
+  const { quizId } = req.params
+
+  if (!quizId) {
+    return sendInvalidInputResponse(res)
+  }
+
+  try {
+    // check if the quiz exists
+    const check:any = await QuizModel.findOne({ _id: quizId, admin: userId });
+    if (!check) {
+      return res.status(404).send({ message: 'Quiz not found' })
+    }
+
+    // update the quiz
+    const quiz = await QuizModel.findOneAndUpdate(
+      { _id: quizId, admin: userId },
+      {
+        $set: {
+          managers,
+          isAcceptingAnswers,
+          quizMetadata,
+          registrationMetadata,
+        },
+      },
+      { new: true }
+    )
+    
+    // send the response back
+    if(!quiz) {
+      return res.status(404).send({ message: 'Quiz not found' })
+    } else {
+      return res.status(200).send({ message: 'Quiz updated', quiz })
+    }
+  } catch (error: unknown) {
+    sendFailureResponse({
+      res,
+      error,
+      messageToSend: 'Failed to update quiz',
+    })
+  }
 }
 
 export default updateQuiz
