38e9f6cc4a5bfd5bf98febc821dd4eab97cb3a08
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-03-14 15:54:04 +0530

    minor changes

--- src/controllers/auth/getGoogleAuth.ts
+++ src/controllers/auth/getGoogleAuth.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 
-const getGoogleAuth = async (req:Request ,res: Response) => {
+const getGoogleAuth = async (req: Request ,res: Response) => {
     return res.redirect(process.env.GOOGLE_AUTH_URL!)
 }
 
