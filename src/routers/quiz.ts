a4448d8a3d39fe821544687fe6db3d7e30601469
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-03-14 16:38:07 +0530

    fix auth middleware

--- src/routers/quiz.ts
+++ src/routers/quiz.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import * as quizController from '@controllers/quiz'
+import { isAuth } from '@utils/isAuth'
 
 const router = express.Router()
 
-router.post('/create', quizController.createQuiz)
+router.post('/create', isAuth, quizController.createQuiz)
 
 export default router
--- src/utils/isAuth.ts
+++ src/utils/isAuth.ts
@@ -1,24 +1,18 @@
 import { verifyToken } from '@utils/token';
 import { Request, Response, NextFunction } from 'express';
-import { JwtPayload } from 'types';
 import sendUnauthorizedResponse from '@utils/unauthorisedResponse';
 
-interface authRequest extends Request {
-    user: JwtPayload
-}
 
-export const isAuth = (req: authRequest, res: Response, next: NextFunction) => {
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.jwt;
     if (!token) {
         return sendUnauthorizedResponse(res);
     }
     try {
         const user = verifyToken(token);
-        req.user = user;
+        req.body.user = user;
         next();
     } catch (error) {
         return sendUnauthorizedResponse(res);
     }
-
-    next();
 }
