9339e8175cd84e8576a35ec8288ab3f1b6e32998
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-04-13 05:17:24 +0530

    fix: disable non-null assertion

--- src/controllers/auth/getGithubAuth.ts
+++ src/controllers/auth/getGithubAuth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 export const getGithubAuth = async (req: Request, res: Response) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return res.redirect(process.env.GITHUB_AUTH_URL!);
 }
 
