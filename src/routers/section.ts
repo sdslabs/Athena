2059506fd9a469a501599932808f7459db71d740
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-04-13 05:12:30 +0530

    fix(section): add auth middleware and fix order of operation

--- src/routers/section.ts
+++ src/routers/section.ts
@@ -1,11 +1,12 @@
-  import express from 'express'
+import express from 'express'
 import * as sectionController from '@controllers/section'
+import { isAuth } from '@utils/isAuth'
 
 const router = express.Router()
 
-router.post('/:quizId', sectionController.createSection)
-router.delete('/:quizId', sectionController.deleteSection)
-router.get('/:quizId', sectionController.getSection)
-router.put('/:quizId', sectionController.updateSection)
+router.post('/:quizId', isAuth, sectionController.createSection)
+router.delete('/:quizId', isAuth, sectionController.deleteSection)
+router.get('/:quizId', isAuth, sectionController.getSection)
+router.put('/:quizId', isAuth, sectionController.updateSection)
 
 export default router
