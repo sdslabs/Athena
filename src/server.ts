6dc273f6e5381c819c11ba3c1ab17fab59c8e422
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-05-23 02:33:52 +0530

    feat: add initial logger configs and remove redundant files

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, NextFunction, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from '@db/connectDB'
 import quizRouter from '@routers/quiz'
@@ -6,6 +6,9 @@ import authRouter from '@routers/auth'
 import sectionRouter from '@routers/section'
 import questionRouter from '@routers/question'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import mongoSanitize from 'express-mongo-sanitize'
+import logger from '@utils/logger'
 
 // Initialize server
 dotenv.config()
@@ -18,6 +21,17 @@ const port = process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(mongoSanitize());
+app.use(morgan('dev'));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if(res.statusCode === 200) {
+  logger.info(`${req.method} ${req.url}`)
+  }
+  next()
+})
+
+
+//cors handling to be done
 
 // Routers
 app.use('/quiz', quizRouter)
---
+++ src/utils/logger.ts
@@ -0,0 +1,45 @@
+import winston from "winston";
+const loggerConfig = {
+	infoFile: process.env.INFO_LOG_PATH || 'logs/info.log',
+	errorFile: process.env.ERROR_LOG_PATH || 'logs/error.log',
+	debugFile: process.env.DEBUG_LOG_PATH || 'logs/debug.log',
+	console: process.env.LOG_ENABLE_CONSOLE || true,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const createTransports = (config:any) => {
+	const customTransports = [
+    new winston.transports.File({
+      filename: config.infoFile,
+      level: 'info',
+    }),
+    new winston.transports.File({
+      filename: config.errorFile,
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: config.debugFile,
+      level: 'debug',
+    })
+  ];
+
+	return customTransports;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const create = (config:any) => new winston.createLogger({
+  transports: createTransports(config),
+	format: winston.format.combine(
+		winston.format.colorize({ all: true }),
+		winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+		winston.format.label({ label: 'Quizio Backend' }),
+		winston.format.printf(
+			(info) => `${info.label} (${info.timestamp}) [${info.level}] : ${info.message}`,
+		),
+	),
+});
+
+
+const logger = create(loggerConfig);
+
+export default logger;
