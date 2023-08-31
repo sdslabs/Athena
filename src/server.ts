670e1b1c7452a5b6e449f7a7a0d6d0c89000cfbd
demigod <divyanshjain.2206@gmail.com>
2023-08-31 07:22:22 +0530

    [WIP] implement timer in react

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
 import { connectDB } from '@db/connectDB'
 import staticRouter from '@routers/static'
 import quizRouter from '@routers/quiz'
@@ -10,7 +13,7 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import mongoSanitize from 'express-mongo-sanitize'
 import logger from '@utils/logger'
-
+import timerService from './services/timer'
 // Initialize server
 dotenv.config()
 connectDB()
@@ -19,35 +22,49 @@ const app: Express = express()
 const port = process.env.PORT
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(mongoSanitize());
-app.use(morgan('dev'));
+app.use(mongoSanitize())
+app.use(morgan('dev'))
+app.use(cors())
 
-// cors remaning to be added
+//socket.io
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+io.on('connection', (socket) => {
+  logger.silly(`⚡️[server]: User connected with socketId: ${socket.id}`)
+  console.log(`⚡️[server]: User connected with socketId: ${socket.id}`)
+  timerService(io, socket)
+})
+
 
 // Middleware to access response body and log accordingly
 app.use((req, res, next) => {
   // Override the res.send method to access the response
-  const originalSend = res.send;
+  const originalSend = res.send
   res.send = function (body) {
     // Access the response here
-    if(res.statusCode === 401) {
-      logger.warn(`Unauthorized Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
-    }
-    else if(res.statusCode === 500) {
-      logger.error(`Internal Server Error: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
-    }
-    else if(res.statusCode === 400) {
+    if (res.statusCode === 401) {
+      logger.warn(
+        `Unauthorized Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
+      )
+    } else if (res.statusCode === 500) {
+      logger.error(
+        `Internal Server Error: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
+      )
+    } else if (res.statusCode === 400) {
       logger.debug(`Bad Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
     }
     // Call the original send method to send the response to the user
-    originalSend.call(this, body);
-  };
-  next();
-});
-
+    originalSend.call(this, body)
+  }
+  next()
+})
 
 // Routers
 app.use('/static', staticRouter)
@@ -60,9 +77,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   logger.silly(`⚡️[server]: Server is running at http://localhost:${port}`)
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
 
-export default app;
+export default app
--- src/services/timer.ts
+++ src/services/timer.ts
@@ -1,14 +1,5 @@
-import app from 'server'
-import { Server } from 'socket.io'
 import QuizModel from '@models/quiz/quizModel'
-import http from 'http'
-
-const server = http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-})
+import logger from '@utils/logger'
 
 const isQuizAcceptingAnswers = async (quizId: any) => {
   // get quiz from quizId
@@ -38,64 +29,172 @@ const isParticipantGivingQuiz = async (quizId: any, userId: any) => {
   return true
 }
 
-io.on('connection', (socket) => async () => {
-  const { quizId, userId } = socket.handshake.query
-
-  if (!quizId || !userId) {
-    socket.disconnect()
-  }
-
-  // check if the quiz is accepting answers and participant is giving quiz
-  // if not, disconnect the socket
-  const isAcceptingAnswers = await isQuizAcceptingAnswers(quizId)
-  const isGivingQuiz = await isParticipantGivingQuiz(quizId, userId)
-
+async function checkUserQuizStatus(quizId: unknown, userId: unknown) {
+  const isAcceptingAnswers: unknown = await isQuizAcceptingAnswers(quizId)
+  const isGivingQuiz: unknown = await isParticipantGivingQuiz(quizId, userId)
   if (!isAcceptingAnswers || !isGivingQuiz) {
-    socket.disconnect()
+    return false
   }
+  return true
+}
 
-  // join the room
-  // @ts-ignore
-  socket.emit('message', 'Welcome to the server!');
-  socket.join(quizId)
-  const enterQuizTimeStamp = getCurrentTime()
-  const quiz = await QuizModel.findById(quizId)
-  const endQuizTimeStamp=quiz.quizMetadata.endDateTimestamp
-  let timeLeft:number
-  
-  
-  const user = quiz.participants?.findEach((participant) => {
-    if (participant.user === userId) {
-      return participant
-    }
-  })
+// io.on('connection', (socket) => async () => {
+//   const { quizId, userId } = socket.handshake.query
 
-  timeLeft=user.time.left
+//   if (!quizId || !userId) {
+//     socket.disconnect()
+//   }
 
-  //send timeLeft to frontend
+//   // check if the quiz is accepting answers and participant is giving quiz
+//   // if not, disconnect the socket
+//   const isAcceptingAnswers = await isQuizAcceptingAnswers(quizId)
+//   const isGivingQuiz = await isParticipantGivingQuiz(quizId, userId)
 
-  socket.on('disconnect', () => {
-    // on disconnect
-    const timePassed = getCurrentTime() - enterQuizTimeStamp
-    const timeToEnd = endQuizTimeStamp - getCurrentTime()
-    timeLeft = Math.min(timeLeft-timePassed, timeToEnd) 
-    if(timeLeft <= -1000) {
-        // -1000 is because of server delays and transmission delays
-        //manipulation by user
-        // submit
-        //update db
-        user.submitted=true
-        socket.disconnect()
-    } else {
-        //update db
-          user.time.left=timeLeft
-          user.isGivingQuiz=false;
-    }   
+//   if (!isAcceptingAnswers || !isGivingQuiz) {
+//     socket.disconnect()
+//   }
+
+//   // join the room
+//   // @ts-ignore
+//   socket.emit('message', 'Welcome to the server!');
+//   socket.join(quizId)
+//   const enterQuizTimeStamp = getCurrentTime()
+//   const quiz = await QuizModel.findById(quizId)
+//   const endQuizTimeStamp=quiz.quizMetadata.endDateTimestamp
+//   let timeLeft:number
+
+//   const user = quiz.participants?.findEach((participant) => {
+//     if (participant.user === userId) {
+//       return participant
+//     }
+//   })
+
+//   timeLeft=user.time.left
+
+//   //send timeLeft to frontend
+
+//   socket.on('disconnect', () => {
+//     // on disconnect
+//     const timePassed = getCurrentTime() - enterQuizTimeStamp
+//     const timeToEnd = endQuizTimeStamp - getCurrentTime()
+//     timeLeft = Math.min(timeLeft-timePassed, timeToEnd)
+//     if(timeLeft <= -1000) {
+//         // -1000 is because of server delays and transmission delays
+//         //manipulation by user
+//         // submit
+//         //update db
+//         user.submitted=true
+//         socket.disconnect()
+//     } else {
+//         //update db
+//           user.time.left=timeLeft
+//           user.isGivingQuiz=false;
+//     }
+
+//   })
+// })
+
+function getCurrentTime() {
+  const currentTime = new Date()
+  return currentTime
+}
+
+let timeLeft: number
+timeLeft=40000
+
+async function timerService(io, socket) {
+  //frontend must emit join_quiz event with quizId and userId as data
+  let enterQuizTimeStamp: unknown
+  let exitQuizTimestamp: unknown
+  let user: unknown
+
+
+  socket.on('message', function (data) {
+    // socket.emit('time', {'time':'696969'});
+  });
+
+  socket.on('another-message', function (data) {
+    socket.emit('not-news', { hello: 'world' });
+  });
+
+  socket.on('join_quiz', (data) => {
+    // socket.emit('time', {'time':'696969'});
+
+    const { room, username } = data
+    console.log(room)
+    console.log(username)
+    console.log(data);
     
-  })
-})
+    // if (!quizId || !userId) {
+    //   logger.silly('⚡️[server]: UserId or QuizId is empty.')
+    //   console.log('⚡️[server]: UserId or QuizId is empty.')
+    //   socket.disconnect()
+    // }
+    // socket.emit('time', {'time':'696969'});
 
-function getCurrentTime(){
-    const currentTime = new Date()
-    return currentTime
-  }
+    // try {
+    //   const checkUserQuizStatusResult = await checkUserQuizStatus(quizId, userId);
+    //   console.log(checkUserQuizStatusResult);
+    // } catch (error) {
+    //   console.error('Error checking user quiz status:', error);
+    // }
+    // console.log(checkUserQuizStatus)
+    // socket.join(quizId)
+    enterQuizTimeStamp = getCurrentTime()
+    // const quiz = await QuizModel.findById(quizId)
+    // endQuizTimeStamp = quiz.quizMetadata.endDateTimestamp
+
+    // user = quiz.participants?.findEach((participant) => {
+    //   if (participant.user === userId) {
+    //     return participant
+    //   }
+    // })
+
+    // timeLeft = user.time.left
+    
+    socket.emit('sendTime', timeLeft)
+    socket.emit('receive_message', {"message":"yo", "username":"Timer", __createdtime__:getCurrentTime})
+    console.log("Time sent")
+    //send timeLeft to frontend
+  })
+
+  // socket.on('disconnect', () => {
+  //   const timePassed = getCurrentTime() - enterQuizTimeStamp
+  //   const timeToEnd = endQuizTimeStamp - getCurrentTime()
+  //   timeLeft = Math.min(timeLeft - timePassed, timeToEnd)
+  //   if (timeLeft <= -1000) {
+  //     // -1000 is because of server delays and transmission delays
+  //     //manipulation by user
+  //     // submit
+  //     //update db
+  //     user.submitted = true
+  //     socket.disconnect()
+  //   } else {
+  //     //update db
+  //     user.time.left = timeLeft
+  //     user.isGivingQuiz = false
+  //   }
+  // })
+
+  socket.on('stop_time', (data)=>{
+    console.log("Time  stopped: " + data);
+  })
+
+  socket.on('disconnect', (data)=>{
+    console.log("Socket Disconnected: " + data);
+    exitQuizTimestamp=getCurrentTime()
+    timeLeft=timeLeft-(exitQuizTimestamp-enterQuizTimeStamp)
+  })
+}
+
+function Time(io, socket) {
+  // socket.on('message', function (data) {
+  //   socket.emit('time', {'time':'696969'});
+  // });
+
+  // socket.on('another-message', function (data) {
+  //   socket.emit('not-news', { hello: 'world' });
+  // });
+}
+
+export default timerService
