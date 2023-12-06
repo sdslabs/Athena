c75a87a8b156590c130c2f10182772dac8eeaf13
Altoiids <somyachawla0250@gmail.com>
2023-12-06 17:22:57 +0530

    minor fix

--- src/services/timer.ts
+++ src/services/timer.ts
@@ -84,9 +84,9 @@ async function timerService(io, socket) {
       user.time.endQuiz = quiz.quizMetadata.endDateTimestamp.getTime()
       user.time.left = Math.min(user.time.left, user.time.endQuiz -19800000 - (new Date()).getTime())
       console.log(user.time.endQuiz - (new Date()).getTime())
-      console.log(user.time.left)
-      console.log(user.time.endQuiz)
-      console.log((new Date()).getTime())
+      console.log("left", user.time.left)
+      console.log("end", user.time.endQuiz)
+      console.log("present",(new Date()).getTime())
 
       if (user.time.left <= 0) {
         console.log("time problem")
@@ -107,11 +107,19 @@ async function timerService(io, socket) {
           return participant
         }
       })
+      console.log("left2", user.time.left)
       socket.checkQuiz = QuizCode.LeftQuiz
       user.time.left = Math.min(
-        user.time.left - ((new Date()).getTime() + 19800000 - user.time.enterQuiz),
+        user.time.left - ((new Date()).getTime() - user.time.enterQuiz),
         user.time.endQuiz -19800000 - (new Date()).getTime(),
       )
+      console.log("left3", user.time.left)
+      console.log("end3", user.time.endQuiz)
+      console.log("present3",(new Date()).getTime())
+      console.log("enter3",user.time.enterQuiz)
+      console.log("value2", user.time.endQuiz -19800000 - (new Date()).getTime())
+      console.log("value1", user.time.left - ((new Date()).getTime() + 19800000 - user.time.enterQuiz))
+
 
       user.isGivingQuiz = false
       saveQuiz(quiz)
